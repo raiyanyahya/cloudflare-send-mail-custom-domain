84dc84e9c8ea19d3dfbbb901ada1615bff1744c6
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-05-14 16:21:09 +0200

    use js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-
-
-export default {
-  async fetch(request) {
-    return new Response("Hello World!");
-  },
-};
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  return new Response('Hello worker!', {
+    headers: { 'content-type': 'text/plain' },
+  })
+}
