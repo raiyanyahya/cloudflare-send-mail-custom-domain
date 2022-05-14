6a32e10e9e5fc073aad56696237d49770f0180ab
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2022-05-14 16:36:28 +0200

    test mail

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-addEventListener('fetch', event => {
+addEventListener("fetch", event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  let content = "";
+  for( var i of request.headers.entries() ) {
+      content += i[0] + ": " + i[1] + "\n";
+  }
+  let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+      "method": "POST",
+      "headers": {
+          "content-type": "application/json",
+      },
+      "body": JSON.stringify({
+          "personalizations": [
+              { "to": [ {"email": "raiyanyahyadeveloper@gmail.com",
+                      "name": "Test Recipient"}]}
+          ],
+          "from": {
+              "email": "sender@upskaler.com",
+              "name": "Test Sender",
+          },
+          "subject": "Test Subject",
+          "content": [{
+              "type": "text/plain",
+              "value": "Test message content\n\n" + content,
+          }],
+      }),
+  });
+
+  let respContent = "";
+  // only send the mail on "POST", to avoid spiders, etc.
+  if( request.method == "POST" ) {
+      const resp = await fetch(send_request);
+      const respText = await resp.text();
+
+      respContent = resp.status + " " + resp.statusText + "\n\n" + respText;
+
+  }
+
+  let htmlContent = "<html><head></head><body><pre>" +
+      "</pre><p>Click to send message: <form method="post"><input type="submit" value="Send"/></form></p>" +
+      "<pre>" + respContent + "</pre>" +
+      "</body></html>";
+  return new Response(htmlContent, {
+      headers: { "content-type": "text/html" },
   })
 }
